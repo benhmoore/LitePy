b76d005cff1ca88f36551743f81dd8060c76190a
Ben Moore <bhm128@msstate.edu>
2022-07-29 19:06:27 -0500

    Added test call.

--- postgres_test.py
+++ postgres_test.py
@@ -1,10 +1,10 @@
 import psycopg2
 
-conn = psycopg2.connect(database='organization',
-                        host='db.engr.msstate.edu',
-                        user='app_inventory',
-                        password='An)KW9?Sh-o3',
-                        port='5432')
+# conn = psycopg2.connect(database='organization',
+#                         host='db.engr.msstate.edu',
+#                         user='app_inventory',
+#                         password='',
+#                         port='5432')
 
 cursor = conn.cursor()
 
